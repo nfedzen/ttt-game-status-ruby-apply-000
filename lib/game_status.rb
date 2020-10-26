2172e2637e63b7bbda78ccec066f285a59e3b2f1
Nathan Fedzen <nfedzen@hotmail.com>
2020-10-26 21:39:52 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,11 +20,12 @@ def won?(board)
     
     if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
       return win_combination
-    
     end
-    
   end
-  
   return false
-  
+end
+
+def full?(board)
+  if board.include(" ")
+    return false
 end
