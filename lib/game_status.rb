dd6895704ec305e1db0d5d7bd0afa7a106bf66ff
Nathan Fedzen <nfedzen@hotmail.com>
2020-10-26 21:27:27 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,8 +17,9 @@ def won?(board)
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
-    puts board
+    
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      puts win_combination
       return win_combination
       
     else 
